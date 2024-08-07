cc1aedd902c5f0e806fe25789d781d2f7a62551f
Prajakta Khobragade <prajakta.k137@gmail.com>
2024-08-07 16:23:33 +0530

    resolved imgs issue

--- components/MyComponent.jsx
+++ components/MyComponent.jsx
@@ -5,8 +5,8 @@ const MyComponent = () => (
     {/* Local image in the public folder */}
     <Image src="/decix.png" alt="Description" width={500} height={300} />
     <Image src="/bcgunionbank.png" alt="Description" width={500} height={300} />
-    <Image src="/Rwfk.png" alt="Description" width={500} height={300} />
-    <Image src="/Sparklet.png" alt="Description" width={500} height={300} />
+    <Image src="/rwfk.png" alt="Description" width={500} height={300} />
+    <Image src="/sparklet.png" alt="Description" width={500} height={300} />
   </div>
 );
 
--- components/project.tsx
+++ components/project.tsx
@@ -52,6 +52,8 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
+          width={500}
+          height={300}
           className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
         transition 
         group-hover:scale-[1.04]
